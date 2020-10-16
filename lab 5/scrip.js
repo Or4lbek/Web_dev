dcc2ec3b58fcee0e02756c29699ceb9ebaa94eea
Or4lbek <190109012@stu.sdu.edu.kz>
2020-10-17 02:24:12 +0600

    sd
    asd

---
+++ lab 5/scrip.js
@@ -0,0 +1,41 @@
+function change(element){
+            var a = element.innerHTML;
+            switch(a){
+                case "Coffee":
+                    document.getElementById("img").src="coffee.png";
+                    document.getElementById("text1").innerHTML = "Coffee";
+                    break;
+                case "Order online":
+                    document.getElementById("img").src="bag.png.png";
+                    document.getElementById("text1").innerHTML = "Order online";
+                    break;
+                case "About us":
+                    document.getElementById("img").src = "https://pngimg.com/uploads/cup/cup_PNG1996.png";
+                    document.getElementById("text1").innerHTML = "About us";
+                    break;
+                case "Contacts":
+                    document.getElementById("img").src = "logol.png";
+                    document.getElementById("text1").innerHTML = "Contacts";
+
+            }
+            $("#img").animate({ opacity: '0'}, "slow");
+            $("#img").animate({ opacity: '1'}, "slow");
+            $(".text").animate({opacity: '0'}, "slow");
+            $(".text").animate({opacity: '1'}, "slow");
+        }
+function big(element){
+    element.style.fontSize = "30px";
+}
+function def(element){
+    element.style.fontSize = "25px";
+}
+function bigc(element){
+    element.style.width = "55px";
+}
+function smallc(element){
+    element.style.width = "50px";
+}
+function load(){
+    $(".text").animate({opacity: '1'}, "slow");
+    $("#img").animate({opacity: '1'}, "slow");
+}
